874ac5bf735b0ba4d0eb0d909685cad5bc4d1275
mateuszlizun <mateusz@lizun.pl>
2022-04-21 15:33:45 +0200

    Modify stream thumbnail_url and url properties

--- streams/views.py
+++ streams/views.py
@@ -1,7 +1,11 @@
 from django.core.cache import cache
 from django.shortcuts import render
 
+from .utils import set_stream_properties
+
 
 def index(request):
-    streams = cache.get("streams")
+    streams_data = cache.get("streams")
+    streams = [set_stream_properties(s) for s in streams_data]
+
     return render(request, "streams/index.html", {"streams": streams})
